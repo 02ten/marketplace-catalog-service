324084cd1cf37d23acb382a3c7c8ec8d505ed873
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-02-11 14:33:58 +0300

    Initial commit

---
+++ src/test/java/com/marketplace/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.marketplace.catalog;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+
+}
