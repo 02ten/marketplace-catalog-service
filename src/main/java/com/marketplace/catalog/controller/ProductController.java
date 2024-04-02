3cdf0ffbfdf4696c69d3cdfc38a599b3bd441f6c
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-04-02 15:13:50 +0300

    Added traces and logs

--- src/main/java/com/marketplace/catalog/controller/ProductController.java
+++ src/main/java/com/marketplace/catalog/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.marketplace.catalog.controller;
 
 import com.marketplace.catalog.model.Product;
 import com.marketplace.catalog.service.ProductService;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.tag.Tags;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,24 +17,44 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+    private final Tracer tracer;
+
     @GetMapping()
-    public ResponseEntity<List<Product>> getAllProducts(){
+    public ResponseEntity<List<Product>> getAllProducts() {
+        Span span = tracer.buildSpan("Getting all products").start();
+        Tags.HTTP_METHOD.set(span, "GET");
+        Tags.HTTP_URL.set(span, "/api/product");
+        span.finish();
         return ResponseEntity.ok(productService.getAllProducts());
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> getProduct(@PathVariable Long id){
-        try{
-            return ResponseEntity.ok(productService.getProductById(id));
-        }catch (IllegalArgumentException exception){
+    public ResponseEntity<?> getProduct(@PathVariable Long id) {
+        Span span = tracer.buildSpan("Getting product by id").start();
+        Tags.HTTP_METHOD.set(span, "GET");
+        Tags.HTTP_URL.set(span, "/api/product/" + id);
+        try {
+            Product product = productService.getProductById(id);
+            span.finish();
+            return ResponseEntity.ok(product);
+        } catch (IllegalArgumentException exception) {
+            span.finish();
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
         }
+
     }
+
     @GetMapping("/category/{id}")
-    public ResponseEntity<?> getProductsByCategory(@PathVariable Long id){
+    public ResponseEntity<?> getProductsByCategory(@PathVariable Long id) {
+        Span span = tracer.buildSpan("Getting all products").start();
+        Tags.HTTP_METHOD.set(span, "GET");
+        Tags.HTTP_URL.set(span, "/api/product/category/"+id);
         try {
             List<Product> products = productService.getProductsByCategory(id);
+            span.finish();
             return ResponseEntity.ok(products);
-        }catch (IllegalArgumentException exception){
+        } catch (IllegalArgumentException exception) {
+            span.finish();
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
--- src/main/java/com/marketplace/catalog/service/ProductService.java
+++ src/main/java/com/marketplace/catalog/service/ProductService.java
@@ -3,6 +3,7 @@ package com.marketplace.catalog.service;
 import com.marketplace.catalog.model.Product;
 import com.marketplace.catalog.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,19 +11,31 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Log4j
 public class ProductService {
     private final ProductRepository productRepository;
     private final CategoryService categoryService;
     public List<Product> getAllProducts(){
+        log.info("Getting all products");
         return productRepository.findAll();
     }
     public Product getProductById(Long id) {
-        return productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Такого продукта нет"));
+        log.info("Getting product by id = " + id);
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if(optionalProduct.isEmpty()){
+            log.error("No product with such id");
+            throw new IllegalArgumentException("Такого товара нет");
+        }
+        log.info("Successful getting products by id = " + id);
+        return optionalProduct.get();
     }
     public List<Product> getProductsByCategory(Long id){
+        log.info("Getting all products by category id = " + id);
         if(!categoryService.existCategoryById(id)){
+            log.error("Такой категории нет");
             throw new IllegalArgumentException("Такой категории нет");
         }
+        log.info("Successful getting products by category id");
         return productRepository.getProductsByCategory_Id(id);
     }
 
